0305c13389fe090207f0fdd8222489cf36caae02
prashantburande <101568499+prashantburande@users.noreply.github.com>
2022-03-28 14:51:11 +0530

    Add files via upload

---
+++ Stringcomapare.java
@@ -0,0 +1,26 @@
+//Q 2 wap to ask two string values from user and check if they are equal or not 
+package string1;
+import java.util.Scanner;
+public class Stringcomapare
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a name");
+		String s1=sc.next(); 
+		System.out.println("Enter a name");
+		String s2=sc.next();
+		
+		if(s1.equals(s2))
+		{
+			System.out.println("Stering value are same");
+		}
+		else
+		{
+			System.out.println("Stering value are not same");
+		}
+
+	}
+
+}
